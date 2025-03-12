68a5a1be14dd3ee6975d23f3c60581eeee5aecfc
. LOKKKNEEE <nguyenhuuloc5112004@gmail.com>
2025-03-12 14:07:59 +0700

    backend

---
+++ cmu/backend/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./routes/auth");
+dotenv.config();
+const app = express();
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("✅ CONNECTED TO MONGO DB");
+  })
+  .catch(err => {
+    console.error("❌ MongoDB connection error:", err);
+  });
+
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+//ROUTES
+app.use("/v1/auth", authRoute);
+
+
+app.listen(8000, () => {
+  console.log("Server is running");
+});
